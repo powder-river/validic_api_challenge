d98c0a739426b5b037dd91952870b09199509126
Danai Adkisson <dadkisso@live.com>
2015-07-30 21:16:21 -0400

    test that api mock test works, correctly

--- cities.rb
+++ cities.rb
@@ -1,14 +1,14 @@
 require 'httparty'
 class City
-
-
   def initialize
+    @job_data = get_data
+  end
 
-
+  def city_name
+    @job_data[0]["location"]
   end
 
   private def get_data
-  HTTParty.get("https://jobs.github.com/positions.json?location=94101&full_time=true")
+    HTTParty.get("https://jobs.github.com/positions.json?location=94101&full_time=true")
   end
-
 end
--- test.rb
+++ test.rb
@@ -1,14 +1,23 @@
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
-# require 'httparty'
+require './cities.rb'
+require 'httparty'
 # require 'faker'
-# require 'json'
+require 'json'
 # require './current_conditions.rb'
 # require './sun_up_down.rb'
 # require './ten_day_forecast.rb'
 # require './alerts.rb'
 # require './hurricanes.rb'
+
+class SunRiseSet
+  private def get_data
+    JSON.parse(File.open("cities.json").read)
+  end
+end
+
+
 
 class ApplicationTest < Minitest::Test
 
@@ -17,5 +26,8 @@ class ApplicationTest < Minitest::Test
     assert true
   end
 
-
+  def test_display_job_location
+     city = City.new
+     assert_equal "San Francisco", city.city_name
+  end
 end
