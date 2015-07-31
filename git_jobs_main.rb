f2d37f9c84843f3393abf63b7d552f2b611e8a86
Danai Adkisson <dadkisso@live.com>
2015-07-31 10:35:12 -0400

    number of jobs is now shown as a percentage and total jobs searced is displayed at the end

--- git_jobs_main.rb
+++ git_jobs_main.rb
@@ -1,51 +1,76 @@
 require 'httparty'
 require './cities.rb'
 
-
+puts "Searching.....\n"
 # think of putting a loop in here. would save some space, code spelly as is
 language_array = ["ruby","java", "c", "c++", "python", "javascript"]
-
+@total_jobs = []
+@sf_jobs = []
 language_array.each do |x|
   san_francisco = City.new("sf",x)
-  puts " There are #{san_francisco.job_num} #{x.capitalize} jobs in San Francisco "
+  @total_jobs << san_francisco.job_num
+  @sf_jobs << san_francisco.job_num
 end
-
-puts "-" * 50
-puts "\n"
-
+puts @sf_jobs = @sf_jobs.reduce(:+)
 language_array.each do |x|
-  boston = City.new("boston",x)
-  puts " There are #{boston.job_num} #{x.capitalize} jobs in Boston "
+  san_francisco = City.new("sf",x)
+  total = san_francisco.job_num
+  puts "#{((total.to_f/@sf_jobs)*100).round(2)}% of the jobs in San Francisco are #{x.capitalize} jobs. "
+
 end
 
-puts "-" * 50
-puts "\n"
 
-language_array.each do |x|
-  boston = City.new("la",x)
-  puts " There are #{la.job_num} #{x.capitalize} jobs in Los Angeles"
-end
-# la = City.new("la")
-# puts "Los Angeles\n"
-# puts "there are #{la.job_num} jobs available in SF"
-# puts "--Ruby"
-# puts la.job_num
+# puts "-" * 50
+# puts "\n"
+#
+# language_array.each do |x|
+#   boston = City.new("boston",x)
+#   puts " There are #{boston.job_num} #{x.capitalize} jobs in Boston "
+#
+#   @total_jobs << boston.job_num
+#
+# end
 #
 #
-# boulder = City.new("boulder")
-# puts "Denver and Boulder\n"
-# puts "there are #{boulder.job_num} jobs available in SF"
-# puts "--Ruby"
-# puts boulder.job_num
+# puts "-" * 50
+# puts "\n"
 #
-# chicago = City.new("chicago")
-# puts "Chicago\n"
-# puts "there are #{chicago.job_num} jobs available in SF"
-# puts "--Ruby"
-# puts chicago.job_num
+# language_array.each do |x|
+#   la = City.new("la",x)
+#   puts " There are #{la.job_num} #{x.capitalize} jobs in Los Angeles"
+#   @total_jobs << la.job_num
 #
-# ny = City.new("ny")
-# puts "New York\n"
-# puts "there are #{ny.job_num} jobs available in SF"
-# puts "--Ruby"
-# puts ny.job_num
+# end
+#
+# puts "-" * 50
+# puts "\n"
+#
+# language_array.each do |x|
+#   boulder = City.new("boulder",x)
+#   puts " There are #{boulder.job_num} #{x.capitalize} jobs in the Denver/Boulder area. "
+#   @total_jobs << boulder.job_num
+#
+# end
+#
+# puts "-" * 50
+# puts "\n"
+#
+# language_array.each do |x|
+#   chicago = City.new("chicago",x)
+#   puts " There are #{chicago.job_num} #{x.capitalize} jobs in Chicago "
+#   @total_jobs << chicago.job_num
+#
+# end
+#
+# puts "-" * 50
+# puts "\n"
+#
+# language_array.each do |x|
+#   ny = City.new("ny",x)
+#   puts " There are #{ny.job_num} #{x.capitalize} jobs in New York "
+#   @total_jobs << ny.job_num
+#
+# end
+
+
+puts "#{@total_jobs.reduce(:+)} Jobs Searched"
